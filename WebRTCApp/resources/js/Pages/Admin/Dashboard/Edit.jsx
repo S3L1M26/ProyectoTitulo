d52245e9cbe218684ebd7166bb705d25caa772b2
Selim López <sonicdash26@hotmail.com>
2025-03-05 16:50:07 -0300

    trabajando en la actualización de contraseñas

--- WebRTCApp/resources/js/Pages/Admin/Dashboard/Edit.jsx
+++ WebRTCApp/resources/js/Pages/Admin/Dashboard/Edit.jsx
@@ -1,6 +1,8 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import EditSipUserForm from "../UserManagement/EditSipUserForm";
 import { Head } from "@inertiajs/react";
+import ResetUserPassword from "../UserManagement/ResetUserPassword";
+import ResetSipPassword from "../UserManagement/ResetSipPassword";
 
 export default function Edit({ user, sipUser, ps_aor, ps_endpoint }) {
     return (
@@ -17,6 +19,13 @@ export default function Edit({ user, sipUser, ps_aor, ps_endpoint }) {
                 <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
                     <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">           
                         <EditSipUserForm className="mt-8" user={user} sipUser={sipUser} ps_aor={ps_aor} ps_endpoint={ps_endpoint}/>              
+                    </div>
+                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
+                        <ResetUserPassword className="max-w-xl" />
+                    </div>
+
+                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
+                        <ResetSipPassword className="max-w-xl" />
                     </div>
                 </div>
             </div>
---
+++ WebRTCApp/resources/js/Pages/Admin/UserManagement/ResetSipPassword.jsx
@@ -0,0 +1,120 @@
+import InputError from '@/Components/InputError';
+import InputLabel from '@/Components/InputLabel';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextInput from '@/Components/TextInput';
+import { Transition } from '@headlessui/react';
+import { useForm } from '@inertiajs/react';
+import { useRef } from 'react';
+
+
+export default function ResetSipPassword({ className = '' }) {
+    const passwordInput = useRef();
+        const currentPasswordInput = useRef();
+    
+        const {
+            data,
+            setData,
+            errors,
+            put,
+            reset,
+            processing,
+            recentlySuccessful,
+        } = useForm({
+            current_sip_password: '',
+            new_sip_password: '',
+            new_sip_password_confirmation: '',
+        });
+    
+        const updatePassword = (e) => {
+            e.preventDefault();
+    
+            put(route('sip-password.update'), {
+                preserveScroll: true,
+                onSuccess: () => reset(),
+                onError: (errors) => {
+                    if (errors.new_sip_password) {
+                        reset('new_sip_password', 'new_sip_password_confirmation');
+                        passwordInput.current.focus();
+                    }
+    
+                    if (errors.current_sip_password) {
+                        reset('current_sip_password');
+                        currentPasswordInput.current.focus();
+                    }
+                },
+            });
+        };
+
+    return (
+        <section className={className}>
+            <header>
+                <h2 className="text-lg font-medium text-gray-900">
+                    Update SIP Password
+                </h2>
+
+                <p className="mt-1 text-sm text-gray-600">
+                    Ensure your SIP account is using a long, random password to stay
+                    secure.
+                </p>
+            </header>
+
+            <form onSubmit={updatePassword} className="mt-6 space-y-6">
+            
+                <div>
+                    <InputLabel htmlFor="new_sip_password" value="New SIP Password" />
+
+                    <TextInput
+                        id="new_sip_password"
+                        ref={passwordInput}
+                        value={data.new_sip_password}
+                        onChange={(e) => setData('new_sip_password', e.target.value)}
+                        type="password"
+                        className="mt-1 block w-full"
+                        autoComplete="new-password"
+                    />
+
+                    <InputError message={errors.new_sip_password} className="mt-2" />
+                </div>
+
+                <div>
+                    <InputLabel
+                        htmlFor="new_sip_password_confirmation"
+                        value="Confirm SIP Password"
+                    />
+
+                    <TextInput
+                        id="new_sip_password_confirmation"
+                        value={data.new_sip_password_confirmation}
+                        onChange={(e) =>
+                            setData('new_sip_password_confirmation', e.target.value)
+                        }
+                        type="password"
+                        className="mt-1 block w-full"
+                        autoComplete="new-password"
+                    />
+
+                    <InputError
+                        message={errors.password_confirmation}
+                        className="mt-2"
+                    />
+                </div>
+
+                <div className="flex items-center gap-4">
+                    <PrimaryButton disabled={processing}>Save</PrimaryButton>
+
+                    <Transition
+                        show={recentlySuccessful}
+                        enter="transition ease-in-out"
+                        enterFrom="opacity-0"
+                        leave="transition ease-in-out"
+                        leaveTo="opacity-0"
+                    >
+                        <p className="text-sm text-gray-600">
+                            Saved.
+                        </p>
+                    </Transition>
+                </div>
+            </form>
+        </section>
+    );
+}
